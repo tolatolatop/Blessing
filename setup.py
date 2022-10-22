e78d168a3b841e2b120ba4d05789931fe835988d
tolatolatop <tolatolatop@gmail.com>
2022-10-23 00:24:29 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Blessing',
+    version='0.0.1',
+    packages=['blessing'],
+    url='',
+    license='',
+    author='tolatolatop',
+    author_email='tolatolatop@gmail.com',
+    description=''
+)
