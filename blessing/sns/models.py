fd43c99621e076e6040dfef08983f6a93968d252
tolatolatop <tolatolatop@gmail.com>
2022-10-23 21:01:32 +0800

    specify search models

--- blessing/sns/models.py
+++ blessing/sns/models.py
@@ -6,9 +6,9 @@ from django.urls import reverse
 
 
 class Search(models.Model):
-    name: models.CharField(max_length=48, blank=True)
-    query: models.CharField(max_length=255)
-    modified: models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=48, blank=True)
+    query = models.CharField(max_length=255)
+    modified = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('search-detail', kwargs={'pk': self.pk})
