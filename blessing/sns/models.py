88c7b22c1c8b03e36dfceff8ba20bca62d32e6de
tolatolatop <tolatolatop@gmail.com>
2022-10-23 20:53:35 +0800

    add search view

--- blessing/sns/models.py
+++ blessing/sns/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+
+class Search(models.Model):
+    name: models.CharField(max_length=48, blank=True)
+    query: models.CharField(max_length=255)
+    modified: models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('search-detail', kwargs={'pk': self.pk})
--- blessing/sns/views.py
+++ blessing/sns/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView, CreateView
+
+from .models import Search
+
+
+class SearchCreateView(CreateView):
+    model = Search
+    fields = ['name', 'query']
+    template_name = 'search.html'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class SearchDetailView(DetailView):
+    model = Search
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
