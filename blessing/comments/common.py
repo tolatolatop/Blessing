7b8e53da5120a4e275d764985c8c89409104eb98
tolatolatop <tolatolatop@gmail.com>
2022-10-24 06:13:36 +0800

    export excel

---
+++ blessing/comments/common.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/24 5:47
+# @Author  : tolatolatop
+# @File    : common.py
+
+import io
+import json
+from typing import List
+
+import xlsxwriter
+from django.http import FileResponse
+from django.conf import settings
+
+from .models import Tweet, Report
+
+
+def read_headers(file_path):
+    data_dir = settings.STATIC_DIR
+    json_file = data_dir / file_path
+    with json_file.open('r') as f:
+        data = json.load(f)
+    return data
+
+
+def export_excel(request, report_id):
+    report_obj = Report.objects.filter(pk=report_id).first()
+    tweets = Tweet.objects.filter(search=report_obj.search)
+
+    buffer = io.BytesIO()
+    workbook = xlsxwriter.Workbook(buffer)
+    worksheet = workbook.add_worksheet()
+    headers = read_headers("tweet_info.json")
+    len_headers = len(headers)
+    for row, t in enumerate(tweets):
+        for col, h in enumerate(headers):
+            worksheet.write(row, col, getattr(t, h))
+        worksheet.write(row, len_headers, t.last_comments.description)
+    workbook.close()
+    buffer.seek(0)
+
+    return FileResponse(buffer, as_attachment=True, filename='report.xlsx')
--- blessing/comments/urls.py
+++ blessing/comments/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import LabelDetailView, CommentFormView, ReportDetailView
+from .common import export_excel
 
 urlpatterns = [
     path('label/<int:pk>', LabelDetailView.as_view(), name='labelmodel-detail'),
     path('comment', CommentFormView.as_view(), name='comment-form'),
     path('report/<int:pk>', ReportDetailView.as_view(), name='report-detail'),
+    path('report/<int:report_id>/excel', export_excel, name='report-excel'),
 ]
