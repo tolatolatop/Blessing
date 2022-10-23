f6a3aa28c02e38ffa2e5d8e95912861b25cfd372
tolatolatop <tolatolatop@gmail.com>
2022-10-24 04:29:01 +0800

    自动填表功能

--- blessing/static/comment_form.js
+++ blessing/static/comment_form.js
@@ -1,3 +1,15 @@
-function data_choice(did) {
-    console.log('choice:' + did);
+function data_choice(cb, did) {
+    var x = document.querySelector("#id_links");
+    if (cb.checked) {
+        x.value = x.value + did + ','
+        x.value = x.value.replace(/,$/, '')
+    }
+    else {
+        reg = RegExp('^' + did + ',')
+        x.value = x.value.replace(reg, '')
+        reg = RegExp(',' + did + '$')
+        x.value = x.value.replace(reg, '')
+        reg = RegExp(',' + did + ',')
+        x.value = x.value.replace(reg, ',')
+    }
 }
