fe228a9b54f576825cf2b8d49255c26288ed02c8
tolatolatop <tolatolatop@gmail.com>
2022-10-24 00:35:15 +0800

    新增管理页面Tweet

--- blessing/comments/admin.py
+++ blessing/comments/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import LabelModel
+from .models import LabelModel, Tweet
 
 
 @admin.register(LabelModel)
 class LabelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
     pass
