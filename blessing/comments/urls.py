e571554e221943998b32fec29f71ec71405741f1
tolatolatop <tolatolatop@gmail.com>
2022-10-24 04:08:49 +0800

    改变view为ReportDetailView

--- blessing/comments/urls.py
+++ blessing/comments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import LabelDetailView, CommentFormView
+from .views import LabelDetailView, CommentFormView, ReportDetailView
 
 urlpatterns = [
     path('label/<int:pk>', LabelDetailView.as_view(), name='labelmodel-detail'),
     path('comment', CommentFormView.as_view(), name='comment-form'),
-    path('report/<int:pk>', CommentFormView.as_view(), name='report-detail'),
+    path('report/<int:pk>', ReportDetailView.as_view(), name='report-detail'),
 ]
